41d838d34dc8f213f6750e67d8fd43deb05da93d
EDIIIZ <ediz.tefenlilioglu@gmail.com>
2023-09-21 20:48:01 +0200

    Create Delete Menu styling

--- src/routes/admin/menu/+page.svelte
+++ src/routes/admin/menu/+page.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
+	import { superForm, superValidate } from 'sveltekit-superforms/client';
 	import type { PageData } from './$types';
-	import SettingsForm from '$lib/components/MenuForm.svelte';
-	export let data: PageData;
+	import MenuForm from '$lib/components/MenuForm.svelte';
 	import * as Card from '$lib/components/ui/card';
+	import MenuListForm from '$lib/components/MenuListForm.svelte';
+
+	export let data: PageData;
 </script>
 
-<div class="max-w-sm">
+<div class="p-4 flex flex-col gap-4">
+	<div class="text-2xl font-bold text-center">Hinzufügen</div>
 	<Card.Root>
 		<div class="p-6">
-			<SettingsForm form={data.form} />
+			<MenuForm form={data.menuForm} />
+		</div>
+	</Card.Root>
+
+	<div class="text-2xl font-bold text-center">Löschen</div>
+	<Card.Root class="flex flex-col p-4 gap-2">
+		<div class="justify-center p-4 gap-2">
+			<MenuListForm menu={data.menu} form={data.menuList} />
 		</div>
 	</Card.Root>
 </div>
