fa91a6b2d793230c7a46d79871c6087efb8a8dea
EDIIIZ <ediz.tefenlilioglu@gmail.com>
2023-09-21 16:29:35 +0200

    add infomation to a local file

---
+++ src/lib/vars.ts
@@ -0,0 +1,12 @@
+const Info = {
+  email: "info@pasha-grill-wi.de",
+  phone: "+49 151 1964 3297",
+  address: {
+    street: "Dotzheimer Strasse",
+    streetNumber: "12",
+    zipCode: "65185",
+    city: "Wiesbaden",
+  },
+};
+
+export default Info;
