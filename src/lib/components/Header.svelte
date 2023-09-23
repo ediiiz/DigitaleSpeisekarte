b0e3f65270c49773e5de85fbb6f58f76e4fd6658
EDIIIZ <ediz.tefenlilioglu@gmail.com>
2023-09-23 21:32:17 +0200

    rework header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,32 +1,66 @@
 <script>
 	import Info from '$lib/vars';
 	import { slide } from 'svelte/transition';
+	import Fire from '$lib/components/Fire.svelte';
 	let showMenu = false;
+
+	const links = [
+		{
+			name: 'Startseite',
+			href: '/'
+		},
+		{
+			name: 'Speisekarte',
+			href: '/menu'
+		},
+		{
+			name: 'Über uns',
+			href: '/'
+		},
+		{
+			name: 'Kontakt',
+			href: '/'
+		},
+		{
+			name: 'Admin',
+			href: '/admin/menu'
+		}
+	];
 </script>
 
 <header class="bg-gray-800 text-white">
-	<div class="container mx-auto px-8 py-6 flex justify-between items-center">
-		<!-- Logo -->
-		<div class="text-lg font-semibold">
-			<a href="/" class="hover:text-gray-400 text-2xl">Pasha Grill Köfte</a>
+	<div class="container mx-auto px-8 py-6 flex flex-row justify-between items-center">
+		<div class="flex flex-row text-lg font-semibold">
+			<div class="relative scale-[20%] md:scale-[25%] bottom-2 md:bottom-3">
+				<Fire />
+			</div>
+			<a href="/" class="hover:text-gray-400 text-xl md:text-2xl relative left-1 md:left-2 top-1"
+				>PASHA KÖFTE GRILL</a
+			>
 		</div>
 
 		<!-- Navigation Menu for Desktop -->
-		<nav class="hidden md:flex space-x-4">
-			<a href="/" class="hover:text-gray-400">Home</a>
-			<a href="/menu" class="hover:text-gray-400">Menu</a>
-			<a href="/" class="hover:text-gray-400">About</a>
-			<a href="/" class="hover:text-gray-400">Contact</a>
+		<nav class="hidden lg:flex space-x-4 text-lg">
+			{#each links as link}
+				<a href={link.href} class="hover:text-gray-400">{link.name}</a>
+			{/each}
 		</nav>
 
 		<!-- Contact Info -->
-		<div class="hidden md:block">
-			<p class="text-sm">Ruf uns an: <a href="tel:{Info.phone}">{Info.phone}</a></p>
+		<div class="hidden lg:block pl-4">
+			<p class="text-sm md:text-xl flex flex-row">
+				<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
+					><path
+						fill="currentColor"
+						d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24c1.12.37 2.33.57 3.57.57c.55 0 1 .45 1 1V20c0 .55-.45 1-1 1c-9.39 0-17-7.61-17-17c0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1c0 1.25.2 2.45.57 3.57c.11.35.03.74-.25 1.02l-2.2 2.2z"
+					/></svg
+				><a class="animate-pulse" href="tel:{Info.phone}">{Info.phone}</a>
+			</p>
 		</div>
 
 		<!-- Mobile Menu Button -->
 		<button
-			class="md:hidden focus:outline-none focus:border-white"
+			class="lg:hidden focus:outline-none focus:border-white"
 			on:click={() => (showMenu = !showMenu)}
 		>
 			<svg
@@ -43,12 +77,11 @@
 
 	<!-- Mobile Menu -->
 	{#if showMenu}
-		<div transition:slide={{ duration: 300 }} class="bg-gray-700 md:hidden">
-			<nav class="container mx-auto px-8 py-4 space-y-2">
-				<a href="/" class="block hover:text-gray-400">Home</a>
-				<a href="/menu" class="block hover:text-gray-400">Menu</a>
-				<a href="/" class="block hover:text-gray-400">About</a>
-				<a href="/admin/menu" class="block hover:text-gray-400">Contact</a>
+		<div transition:slide={{ duration: 300 }} class="bg-gray-700 lg:hidden">
+			<nav class="container mx-auto px-8 py-4 space-y-2 text-lg">
+				{#each links as link}
+					<a href={link.href} class="block hover:text-gray-400">{link.name}</a>
+				{/each}
 			</nav>
 		</div>
 	{/if}
