402e6c7d620969e679793e3e0603b59c799f8aa5
EDIIIZ <ediz.tefenlilioglu@gmail.com>
2023-09-25 14:00:17 +0200

    add alert component

---
+++ src/lib/components/MessageAlert.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { getForm } from 'formsnap';
+	const { message } = getForm();
+	import { AlertCircle } from 'lucide-svelte';
+	import * as Alert from '$lib/components/ui/alert';
+</script>
+
+{#if $message}
+<div>
+  class:success={message.status === 'success'}
+</div>
+	<Alert.Root variant="default" class="border-green-400 bg-green-300">
+		<AlertCircle class="text-green-700" />
+		<div class="relative top-1 md:top-0.5">
+			<Alert.Description class="text-base md:text-lg">{$message}</Alert.Description>
+		</div>
+	</Alert.Root>
+{/if}
