f144236c0db635c42ac5e73078595cad9752e0ad
EDIIIZ <ediz.tefenlilioglu@gmail.com>
2023-09-25 14:01:48 +0200

    add homepage

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<div class="w-full relative">
+	<img
+		src="https://pmtwnaoblvfsrckhnget.supabase.co/storage/v1/object/public/produktbilder/mainpage/751da966-f1df-4e66-b237-b01d3b724529"
+		alt="img"
+		class="object-cover w-full max-h-[500px]"
+	/>
+	<div
+		class="absolute inset-0 before:content before:block before:absolute before:w-full before:h-[20%] before:top-0 after:content after:block after:absolute after:w-full after:h-[20%] after:bottom-0"
+		style="background: linear-gradient(to bottom, #1F2937 0%, transparent 100%);"
+	></div>
+</div>
