799137022c3ada7ae6851604bd408f9af3f1fd40
EDIIIZ <ediz.tefenlilioglu@gmail.com>
2023-09-27 20:42:31 +0200

    Additives component

---
+++ src/lib/components/Additives.svelte
@@ -0,0 +1,35 @@
+<div class="p-4 flex flex-col text-start">
+	<h3 class="text-xl font-bold mb-4">Inhaltsstoffe:</h3>
+	<ul class="list-disc pl-6 mb-4">
+		<li>1 Antioxidationsmittel & Konservierungstoffe</li>
+		<li>2 Antioxidationsmitteln</li>
+		<li>3 Phosphat</li>
+		<li>4 Konservierungsstoffe</li>
+		<li>5 geschwärzt</li>
+		<li>6 Farbstoff</li>
+		<li>7 Süßungsmittel</li>
+		<li>8 KoffeinhaItig</li>
+		<li>9 Chininhaltig</li>
+		<li>10 Geschmacksverstärker</li>
+	</ul>
+	<h3 class="text-xl font-bold mb-4">Kennzeichnung der Allergene:</h3>
+	<ul class="list-disc pl-6 flex flex-col gap-y-2 mb-4">
+		<li>A Glutenhaltiges Getreide</li>
+		<li>B Krebstiere und daraus gewonnene Erzeugnisse</li>
+		<li>C Eier und daraus gewonnene Erzeugnisse</li>
+		<li>D Fische und daraus gewonnene Erzeugnisse</li>
+		<li>E Erdnüsse und daraus gewonnene Erzeugnisse</li>
+		<li>F Soja(bohnen) und daraus gewonnene Erzeugnisse</li>
+		<li>G Milch und daraus gewonnene Erzeugnisse</li>
+		<li>H Schalenfrüchte</li>
+		<li>I Sellerie und daraus gewonnene Erzeugnisse</li>
+		<li>J Senf und daraus gewonnene Erzeugnisse</li>
+		<li>K Sesamsamen und daraus gewonnene Erzeugnisse</li>
+		<li>L Schwefeldioxid und Sulphite</li>
+		<li>M Lupinen und daraus gewonnene Erzeugnisse</li>
+		<li>N Weichtiere und daraus gewonnene Erzeugnisse</li>
+	</ul>
+	<p class="text-sm italic">
+		Bei Allergien oder Unverträglichkeiten fragen Sie unsere Mitarbeiter.
+	</p>
+</div>
