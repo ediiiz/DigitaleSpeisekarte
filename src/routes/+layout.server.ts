320e0c0509dcb4a0081a0ccd903f4a0ed9475a1e
EDIIIZ <ediz.tefenlilioglu@gmail.com>
2023-09-28 19:39:37 +0200

    remove not working store

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,20 +1,6 @@
-import db from '$lib/server/prisma/prisma';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = (async ({ locals, url: { pathname } }) => {
   const session = await locals.auth.validate()
-  let userStore = undefined;
-  if (session) {
-    const user = await db.user.findUnique({
-      where: {
-        id: session.user.userId
-      }
-    })
-    userStore = user;
-  }
-  if (userStore) {
-    return { session, pathname, userStore };
-  }
-
   return { session, pathname };
-}) satisfies LayoutServerLoad;
+});
